e95330ae4c6d277902f391313273a3c27dd0d7d5
vadim.makerov <vadim.makerov@ispring.com>
2025-11-04 00:53:45 +0300

    Add sendme

--- apps.tf
+++ apps.tf
@@ -73,6 +73,7 @@ locals {
     "tree-sitter",
     "fd",
     "neovim",
+    "sendme",
     # https://github.com/Julien-R44/fast-ssh
     # Not avaiable via brew and for darwin arm64
     # cargo install fast-ssh
