2644bd065c664e74121796433a6ba65f198fc409
UsingCoding <vadimmakerov42@yandex.ru>
2026-01-18 23:20:33 +0300

    feat: set backup configuration

--- backup/export-pass.sh
+++ backup/export-pass.sh
@@ -2,7 +2,7 @@
 
 # Export PASS from `gostore cat pass` if available
 if command -v gostore >/dev/null 2>&1; then
-  if PASS_VALUE=$(gostore cat backup/vadim-mac pass 2>/dev/null); then
+  if PASS_VALUE=$(gostore cat backup/vadim-mac-personal pass 2>/dev/null); then
     if [ -n "$PASS_VALUE" ]; then
       export AUTORESTIC_MICROUSER_RESTIC_PASSWORD="$PASS_VALUE"
     fi
